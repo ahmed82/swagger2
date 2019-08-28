80df2bcf37b7b0ca54059dc89ea83a1a0a16d33d
Ahmed AlSalih <a.alsalih2@gmail.com>
2019-08-28 01:09:51 -0400

    add swagger metadata

--- src/main/java/com/example/demo/DemoProjectApplication.java
+++ src/main/java/com/example/demo/DemoProjectApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,7 +27,16 @@ public class DemoProjectApplication {
         	.select()
             .paths(PathSelectors.ant("/api/*"))
             .apis(RequestHandlerSelectors.basePackage("com.example"))
+            .build().apiInfo(apiEndPointsInfo());
+    }
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+            .description("Address Book Management REST API")
+            .license("ATR com")
+            .licenseUrl("http://www.google.com")
+            .version("1.7.0")
             .build();
+    
     }
 
 }
