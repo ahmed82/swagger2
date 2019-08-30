03639282bfb3b82ed274108bc06300a6c85c6e23
Ahmed AlSalih <a.alsalih2@gmail.com>
2019-08-29 22:27:04 -0400

    move the swagger configuration to separate file

--- src/main/java/com/example/demo/Swagger2Config.java
+++ src/main/java/com/example/demo/Swagger2Config.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -10,24 +11,29 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2	
+@Configuration
+@EnableSwagger2	
 public class Swagger2Config {
 	
-	 //@Bean
+	 @Bean
 	    public Docket api() {
-	        return new Docket(DocumentationType.SWAGGER_2).select()
-	            .apis(RequestHandlerSelectors
-	                .basePackage("net.guides.springboot2.springboot2swagger2.controller"))
-	            .paths(PathSelectors.regex("/.*"))
-	            .build().apiInfo(apiEndPointsInfo());
+	        return new Docket(DocumentationType.SWAGGER_2)
+	        		.select()
+	        		.paths(PathSelectors.ant("/api/*"))
+	        		//.paths(PathSelectors.regex("/.*"))
+	                .apis(RequestHandlerSelectors.basePackage("com.example"))
+	                .build().apiInfo(apiEndPointsInfo());
+	            
+
+
 	    }
 	    private ApiInfo apiEndPointsInfo() {
 	        return new ApiInfoBuilder().title("Spring Boot REST API")
-	            .description("Employee Management REST API")
-	            .license("Apache 2.0")
-	            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-	            .version("1.0.0")
-	            .build();
+	        		.description("Address Book Management REST API")
+	                .license("ATR com")
+	                .licenseUrl("http://www.google.com")
+	                .version("1.7.0")
+	                .build();
 	    }
 
 }
